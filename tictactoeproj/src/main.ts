cde9bde3077727b613760a0e32413a7e6dbbe115
Chelsea Li <148104000+Chelsealily@users.noreply.github.com>
2023-12-06 13:32:07 +0000

    minor bug fixed. need to add winning constants

--- tictactoeproj/src/main.ts
+++ tictactoeproj/src/main.ts
@@ -11,55 +11,13 @@ if (!mainGrid || !messageArea || !messageText || !restartButton) {
   }
 
 // defaults
-let o_Player = true;
+const xMove = "X"
+const oMove = "O"
+let currentPlayer = "O";
 let startState = ["", "", "", "", "", "", "", "", ""];
-let gameStarted = true;
+let gameStarted = false;
 
-// handle start message
-if (startState=["", "", "", "", "", "", "", "", ""]) {
-    messageText.innerText = "Choose a square to start the game! ☝️ "
-}
-
-
-// handle box click - NEED TO ADD PLAYER CHANGE 
-const handleBoxClick = (event: Event) => {
-    const clickedBox = event.currentTarget as HTMLElement;
-    const clickedBoxNumber = Number(clickedBox.getAttribute("id"));
-   
-    if (startState[clickedBoxNumber] == "" || !gameStarted) {
-       clickedBox.innerText= "O";
-       o_Player=false;
-       //console.log(o_Player);
-       messageText.innerText = "";
-  } else {
-    //prompt("That square has already been used");
-    return;
-  }
-};
-
-gridBoxes.forEach(button => {
-    button.addEventListener("click", handleBoxClick);
-});
-
-
-// handle restart game
-
-const handleRestartGame = (event: Event) => {
-    startState = ["", "", "", "", "", "", "", "", ""]
-    gameStarted = false;
-    gridBoxes.forEach(box => box.innerHTML = "")
-    messageText.innerText = "Choose a square to start the game! :D "
-}
-
-restartButton.addEventListener("click", handleRestartGame);
-
-
-
-/*
-
-// constants
-
-const winningCombos = [
+/*const winningCombos = [
 	[0, 1, 2],
 	[3, 4, 5],
 	[6, 7, 8],
@@ -68,7 +26,112 @@ const winningCombos = [
 	[2, 5, 8],
 	[0, 4, 8],
 	[2, 4, 6] 
-]
+]*/
+
+// handle start message
+if (startState=["", "", "", "", "", "", "", "", ""]) {
+    messageText.innerText = "Pick a square to Start! ☝️ "
+}
+
+
+// handle player clicks
+const handlePlayerClick = (clickedBox, index) => {
+    if(clickedBox.innerHTML === "" && currentPlayer){
+        updateBoard(clickedBox, index);           
+        changePlayer();
+        messageText.innerText = "";  
+        gameStarted = true;
+        //resultCheck(); 
+    } else {
+       //handleComputerTurn();
+       return;
+    }
+}
+
+    gridBoxes.forEach((clickedBox, index) => 
+        clickedBox.addEventListener("click", () => {
+            handlePlayerClick(clickedBox, index);
+    }));
+
+ /* computers turn 
+
+const handleComputerTurn = (clickedBox, index)  => {
+    const random = Math.floor(Math.random() * gridBoxes.length);
+ 
+    if (currentPlayer="X") {
+
+        if (clickedBox.innerHTML === "") {
+        gridBoxes[random].innerHTML = xMove;
+        currentPlayer = "O";    
+      } else {
+      return
+    
+}
+}}*/
+
+
+//update board
+const updateBoard = (clickedBox, index) => {
+    startState[index] = currentPlayer;
+    clickedBox.innerHTML = currentPlayer;
+    clickedBox.classList.add(currentPlayer);
+    console.log(clickedBox.classList)
+}
+
+// change players
+const changePlayer = () => {
+    if(currentPlayer === "O") {
+        gridClassSwap();
+        currentPlayer = "X";
+    } else {
+        gridClassSwap();
+        currentPlayer = "O";
+    }
+}
+
+// Change computer turn class
+const gridClassSwap = () => {
+    if(mainGrid.classList.contains("X")) {
+        mainGrid.classList.remove(xMove);
+        mainGrid.classList.add(oMove);
+    } else {
+        mainGrid.classList.remove(oMove);
+        mainGrid.classList.add(xMove);
+    }
+}
+
+
+/*reset individual boxes
+const resetBoxData = (clickedBox) => {
+    clickedBox.innerHTML = "";
+    clickedBox.classList.remove(xMove);
+    clickedBox.classList.add(oMove);
+}*/
+
+
+// handle restart game
+
+const handleRestartGame = (event: Event) => {
+    startState = ["", "", "", "", "", "", "", "", ""]
+    gameStarted = false;
+    currentPlayer="O";
+    gridBoxes.forEach(box => box.innerHTML = "")
+    messageText.innerText = "Choose a square to start the game! :D "
+    mainGrid.classList.remove(xMove);
+    mainGrid.classList.add(oMove);
+    //gridBoxes.forEach((clickedBox) => resetBoxData(clickedBox));
+
+}
+
+restartButton.addEventListener("click", handleRestartGame);
+
+
+
+/*// constants
+
+// check rows,columns diagonals
+
+
 
 const compTurn = spot []
 let p1 = [2, 4]
